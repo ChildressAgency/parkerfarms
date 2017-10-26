8057a19ea1408f09b0fcdb83cb52c4aa80cec240
childressdev <jcampbell@childressagency.com>
2017-10-26 17:29:48 -0400

    about demo styled, timeline programmed

--- js/parkerfarms-scripts.js
+++ js/parkerfarms-scripts.js
@@ -1,3 +1,26 @@
 jQuery(document).ready(function($){
   
+  $('.timeline-wrapper').on('scroll', function(){
+    var st = $(this).scrollTop();
+    $('.fademe').each(function(){
+      //console.log($(this).position());
+
+      if($(this).position().top < 150){
+          //$(this).animate({opacity : 1});
+          $(this).addClass('fade-zero');
+          $(this).removeClass('fade-one');
+          $(this).removeClass('fade-two');
+      }
+      else if($(this).position().top < 400){
+        //$(this).animate({opacity : .66});
+        $(this).addClass('fade-one');
+        $(this).removeClass('fade-two');
+      }
+      else{
+        //$(this).animate({opacity : .33});
+        $(this).addClass('fade-two');
+      }
+    });
+  });
+
 });
